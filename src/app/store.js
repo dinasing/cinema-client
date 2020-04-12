4c27361a6d564364deacb8c534c3a565a24834db
Dina Singatulina <dina.sing1212@gmail.com>
2020-04-12 17:43:09 +0300

    refactor: delete redux-thunk

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,5 @@
 /* eslint-disable */
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import thunk from 'redux-thunk';
-import { compose, createStore } from 'redux';
 import rootReducer from './reducers/rootReducer';
 
 export const store = configureStore({
