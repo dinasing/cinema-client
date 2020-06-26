4a33f284ae87f90bcf1b2b192ff31edbd9bca35e
Dina Singatulina <dina.sing1212@gmail.com>
2020-06-26 13:18:34 +0300

    feat: create HOC for 2nd level menu

---
+++ src/app/menu/menuItemsConstants.js
@@ -0,0 +1,13 @@
+export const MOVIES_MENU_ITEMS = [
+  { link: 'movies', name: 'all movies' },
+  { link: 'movies/add', name: 'add movie' },
+  { link: 'movies/edit', name: 'edit movie' },
+  { link: 'movies/delete', name: 'delete movie' },
+];
+
+export const CINEMAS_MENU_ITEMS = [
+  { link: 'movie-theaters', name: 'all movie theatres' },
+  { link: 'movie-theaters/add', name: 'add movie theatres' },
+  { link: 'movie-theaters/edit', name: 'edit movie theatre' },
+  { link: 'movie-theaters/delete', name: 'delete movie theatre' },
+];
