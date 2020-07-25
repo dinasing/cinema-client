4e2c337630cf5afd500f664b66e7806b275ae279
Dina Singatulina <dina.sing1212@gmail.com>
2020-07-25 13:16:43 +0300

    feat: add cancel delete modal function

--- src/app/cinema/components/CinemaHallsContainer.js
+++ src/app/cinema/components/CinemaHallsContainer.js
@@ -2,23 +2,60 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { Button, Modal, ModalFooter, ModalHeader } from 'reactstrap';
 import HallSchema from './HallSchema';
 import { getSeatTypes } from '../../seatType/actions/seatTypeAction';
+import { deleteCinemaHall } from '../actions/cinemaHallAction';
 
 class CinemaHallContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isDeleteModalOpen: false,
+    };
+  }
+
   componentDidMount() {
     this.props.getSeatTypes();
   }
+
+  toggleDeleteModal = hallIdToDelete => () => {
+    this.setState(prevState => ({
+      isDeleteModalOpen: !prevState.isDeleteModalOpen,
+      hallIdToDelete,
+    }));
+  };
+
+  cancelModal = () => {
+    this.setState({
+      isDeleteModalOpen: false,
+      hallIdToDelete: null,
+    });
+  };
+
+  deleteCinemaHallHandle = () => {
+    const hallId = this.state.hallIdToDelete;
+
+    this.props.deleteCinemaHall(hallId);
+    this.setState({
+      isDeleteModalOpen: false,
+      hallIdToDelete: '',
+    });
+  };
 
   render() {
     let { cinemaHalls } = this.props.cinemaHalls;
     const { seatTypes } = this.props.seatsTypes;
     const cinemaId = this.props.match.params.id;
+    const { isDeleteModalOpen } = this.state;
     cinemaHalls = cinemaHalls.filter(hall => this.props.match.params.id == hall.cinemaId);
     const cinemaHallsComponent = cinemaHalls.map(hall => {
       return (
         <>
-          <p>hall {hall.title}</p>
+          <p>
+            hall {hall.title} <Link onClick={this.toggleDeleteModal(hall.id)}>delete</Link>
+          </p>
           <HallSchema schema={hall.schema} hallTitle={hall.title} />
           <p>
             seats types:{' '}
@@ -36,6 +73,18 @@ class CinemaHallContainer extends Component {
     return (
       <>
         <h3>
+          <Modal isOpen={isDeleteModalOpen} toggle={this.toggleDeleteModal}>
+            <ModalHeader>Are you sure you want to delete cinema hall?</ModalHeader>
+            <ModalFooter>
+              {' '}
+              <Button onClick={this.deleteCinemaHallHandle} color="danger">
+                delete
+              </Button>{' '}
+              <Button onClick={this.cancelModal} color="primary">
+                cancel
+              </Button>
+            </ModalFooter>
+          </Modal>
           cinema halls{' '}
           <small>
             <Link to={'/movie-theaters/' + cinemaId + '/add-hall/'}>add hall</Link>
@@ -56,4 +105,4 @@ const mapStateToProps = state => ({
   cinemaHalls: state.rootReducer.cinemaHall,
   seatsTypes: state.rootReducer.seatType,
 });
-export default connect(mapStateToProps, { getSeatTypes })(CinemaHallContainer);
+export default connect(mapStateToProps, { getSeatTypes, deleteCinemaHall })(CinemaHallContainer);
